ce80dba14db986a169aab77f346d653dd90d54f9
Usama <uanwar292@gmail.com>
2022-11-12 10:59:45 +0500

    signIn complete

---
+++ config.js
@@ -0,0 +1,17 @@
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+//   import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyA5cuEDejR99HedYWktbRuYAboZINN8Z0w",
+    authDomain: "hackaton-e7f62.firebaseapp.com",
+    projectId: "hackaton-e7f62",
+    storageBucket: "hackaton-e7f62.appspot.com",
+    messagingSenderId: "966273044023",
+    appId: "1:966273044023:web:461e406ceb69395bf039b2",
+    measurementId: "G-VEVKJCKX9R"
+  };
+
+
+  const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
---
+++ signin.js
@@ -0,0 +1,30 @@
+import {app} from "./config.js"
+import { getAuth, signInWithEmailAndPassword, } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+
+let btn = document.querySelector("#btn");
+btn.addEventListener("click",signIn);
+
+function signIn(){
+
+const auth = getAuth(app);
+signInWithEmailAndPassword(auth, email.value, password.value)
+.then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log("user login successfully",user);
+    
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorMessage,errorCode);
+    console.log("something went wrong",error);
+    // ..
+
+    
+  });
+}
